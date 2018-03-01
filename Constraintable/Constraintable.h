becfeb4b925e74f6f1c01c3db0bb0179483cd97d
Sergei Golishnikov <sergeymild@yandex.ru>
2018-03-01 21:25:27 +0200

    Initial Commit

---
+++ Constraintable/Constraintable.h
@@ -0,0 +1,19 @@
+//
+//  Constraintable.h
+//  Constraintable
+//
+//  Created by Sergey Golishnikov on 01/03/2018.
+//  Copyright © 2018 Sergey Golishnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Constraintable.
+FOUNDATION_EXPORT double ConstraintableVersionNumber;
+
+//! Project version string for Constraintable.
+FOUNDATION_EXPORT const unsigned char ConstraintableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Constraintable/PublicHeader.h>
+
+
